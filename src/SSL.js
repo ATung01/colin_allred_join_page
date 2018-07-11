295aca2365839b9cf88a90833f9bfcb649a42e22
Atung01 <Acc.tung@gmail.com>
2018-07-11 17:01:04 -0500

    react router added for SSL Letsencrypt

---
+++ src/SSL.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class SSL extends Component {
+  render() {
+    return (
+      <div>xxxxxxxxxxxx-yyyy.zzzzzzzzzzzzzzzzzzz</div>
+    )
+  }
+}
+
+export default SSL;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import SSL from './SSL';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render((
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route exact path="/.well-known/acme-challenge/:content" component={SSL} />
+    </div>
+  </Router>
+), document.getElementById('root'))
